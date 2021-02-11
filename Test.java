341533ecd8ede230338911c0ef49230ff3508c97
Dennis Dang <dennisdang17@gmail.com>
2021-02-11 10:27:15 -0800

    Finished template

---
+++ Test.java
@@ -0,0 +1,27 @@
+/**
+ * Test your code inside of this class as I will be using an 
+ * autograder with your WiggleSort functions.
+ *
+ * @author 
+ * 
+ * Test inputs: 
+ * 1,3,2,2,3,1
+ * 3,5,2,1,6,4
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        int nums[] = {1,5,1,1,6,4}; //replace these numbers to test
+
+        System.out.println("Nums before wiggle sort: ");
+        for(int i=0; i< nums.length; i++){
+            System.out.print(nums[i]);
+        }   
+
+        System.out.println("\nNums after wiggle sort: ");
+        int test[] = WiggleSort.wiggleSort(nums); //Calls wigglesort
+        for(int i=0; i< test.length; i++){
+            System.out.print(test[i]);
+        }   
+    }
+}
---
+++ WiggleSort.java
@@ -0,0 +1,11 @@
+
+/**
+ * Sorts an array into WiggleSort format
+ *
+ * @author 
+ */
+public class WiggleSort {
+    public static int[] wiggleSort(int[] nums) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
